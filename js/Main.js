286e2929dc35f84d0236f7e9059543301da3d8d2
siinch <simon.ingemanns@protonmail.com>
2020-02-25 15:12:11 +0100

    rmlegalmovesutility

--- js/Main.js
+++ js/Main.js
@@ -2,7 +2,6 @@ require.config({
 
   paths: {
     PieceData: "data/PieceData",
-    LegalMovesUtility: "data/LegalMovesUtility",
     PieceControl: "control/PieceControl",
     UnitTests: "test/UnitTests"
   }
--- js/data/PieceData.js
+++ js/data/PieceData.js
@@ -1,4 +1,4 @@
-define(["PieceUtility"], function(pu) {
+define(["LegalMovesUtility"], function(lmu) {
 
   var pieces = CreatePieces();
 
@@ -43,6 +43,111 @@ define(["PieceUtility"], function(pu) {
     switch (this.type) {
       case "rook": return GetLegalMovesRook(this); break;
     }
+  }
+
+  function GetLegalMovesRook (piece) {
+    var legalMoves = [];
+    piece.position = "5d";
+    legalMoves = legalMoves.concat(lmu.GetAllUp(piece));
+    legalMoves = legalMoves.concat(lmu.GetAllDown(piece));
+    legalMoves = legalMoves.concat(lmu.GetAllLeft(piece));
+    legalMoves = legalMoves.concat(lmu.GetAllRight(piece));
+    console.log(legalMoves);
+    return legalMoves;
+  }
+
+  // Helper functions
+
+  function GetAllDown (piece) {
+    var columns = ["a", "b", "c", "d", "e", "f", "g", "h"];
+    var legalMoves = [];
+    var letter = piece.position[0];
+    var column = columns.indexOf(piece.position[0]);
+    var row = parseInt(piece.position[1]);
+
+    // scan all the positions below the current position
+    for(var r = row -1; r > 0; r--) {
+      var pos = letter + r;
+      // check for other pieces
+      var otherPiece = pieces.find(x => x.position === pos);
+      if (otherPiece === undefined)
+        legalMoves.push(pos);
+      else {
+        // legal if other color and stop
+        if(otherPiece.color != piece.color)
+          legalMoves.push(pos);
+        break;
+      }
+    }
+    return legalMoves;
+  }
+
+  function GetAllUp (piece) {
+    var columns = ["a", "b", "c", "d", "e", "f", "g", "h"];
+    var legalMoves = [];
+    var letter = piece.position[0];
+    var column = columns.indexOf(piece.position[0]);
+    var row = parseInt(piece.position[1]);
+
+    for(var r = row + 1; r < 9; r++) {
+      var pos = letter + r;
+      var otherPiece = pieces.find(x => x.position === pos);
+      if (otherPiece === undefined)
+        legalMoves.push(pos);
+      else {
+        if(otherPiece.color != piece.color)
+          legalMoves.push(pos);
+        break;
+      }
+    }
+
+    return legalMoves;
+  }
+
+  function GetAllRight (piece) {
+    var columns = ["a", "b", "c", "d", "e", "f", "g", "h"];
+    var legalMoves = [];
+    var letter = piece.position[0];
+    var column = columns.indexOf(piece.position[0]);
+    var row = parseInt(piece.position[1]);
+
+    // scan right
+    for(var c = column + 1; c < 8; c++) {
+      var pos = columns[c] + row;
+      var otherPiece = pieces.find(x => x.position === pos);
+      if (otherPiece === undefined)
+        legalMoves.push(pos);
+      else {
+        if(otherPiece.color != piece.color)
+          legalMoves.push(pos);
+        break;
+      }
+    }
+
+    return legalMoves;
+  }
+
+  function GetAllLeft (piece) {
+    var columns = ["a", "b", "c", "d", "e", "f", "g", "h"];
+    var legalMoves = [];
+    var letter = piece.position[0];
+    var column = columns.indexOf(piece.position[0]);
+    var row = parseInt(piece.position[1]);
+
+    // scan left
+    for(var c = column - 1; c > -1; c--) {
+      var pos = columns[c] + row;
+      var otherPiece = pieces.find(x => x.position === pos);
+      if (otherPiece === undefined)
+        legalMoves.push(pos);
+      else {
+        if(otherPiece.color != piece.color)
+          legalMoves.push(pos);
+        break;
+      }
+    }
+
+    return legalMoves;
   }
 
   return {
